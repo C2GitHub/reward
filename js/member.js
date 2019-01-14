41311140a38e82fc29940504ad81d473c88d2933
Appleseed <18829028151@163.com>
2019-01-14 11:08:25 +0800

    first init

---
+++ js/member.js
@@ -0,0 +1,363 @@
+var member = [
+  {
+    "sid": 1,
+    "depart": "采购中心",
+    "name": "陆淑红"
+  },
+  {
+    "sid": 2,
+    "depart": "革新中心",
+    "name": "岑群育"
+  },
+  {
+    "sid": 3,
+    "depart": "采购中心",
+    "name": "陈贞儿"
+  },
+  {
+    "sid": 4,
+    "depart": "制造一部",
+    "name": "马季"
+  },
+  {
+    "sid": 5,
+    "depart": "PMC中心",
+    "name": "刘少华"
+  },
+  {
+    "sid": 6,
+    "depart": "制造技术",
+    "name": "韩旭东"
+  },
+  {
+    "sid": 7,
+    "depart": "PMC中心",
+    "name": "胡爱波"
+  },
+  {
+    "sid": 8,
+    "depart": "PMC中心",
+    "name": "陆波"
+  },
+  {
+    "sid": 9,
+    "depart": "PMC中心",
+    "name": "陆建达"
+  },
+  {
+    "sid": 10,
+    "depart": "喷粉车间",
+    "name": "刘赛波"
+  },
+  {
+    "sid": 11,
+    "depart": "设备中心",
+    "name": "王钦"
+  },
+  {
+    "sid": 12,
+    "depart": "PMC中心",
+    "name": "张新强"
+  },
+  {
+    "sid": 13,
+    "depart": "粘胶部装车间",
+    "name": "陈康"
+  },
+  {
+    "sid": 14,
+    "depart": "采购中心",
+    "name": "周伟峰"
+  },
+  {
+    "sid": 15,
+    "depart": "冲制车间",
+    "name": "罗克清"
+  },
+  {
+    "sid": 16,
+    "depart": "革新中心",
+    "name": "徐建治"
+  },
+  {
+    "sid": 17,
+    "depart": "革新中心",
+    "name": "雷敏"
+  },
+  {
+    "sid": 18,
+    "depart": "制造技术",
+    "name": "张立平"
+  },
+  {
+    "sid": 19,
+    "depart": "精加工车间",
+    "name": "周青伟"
+  },
+  {
+    "sid": 20,
+    "depart": "PMC中心",
+    "name": "李杨"
+  },
+  {
+    "sid": 21,
+    "depart": "革新中心",
+    "name": "胡建燕"
+  },
+  {
+    "sid": 22,
+    "depart": "制造技术",
+    "name": "李健"
+  },
+  {
+    "sid": 23,
+    "depart": "喷漆车间",
+    "name": "郑志浩"
+  },
+  {
+    "sid": 24,
+    "depart": "PMC中心",
+    "name": "马孟军"
+  },
+  {
+    "sid": 25,
+    "depart": "采购中心",
+    "name": "林凤"
+  },
+  {
+    "sid": 26,
+    "depart": "革新中心",
+    "name": "高九江"
+  },
+  {
+    "sid": 27,
+    "depart": "PMC中心",
+    "name": "李超"
+  },
+  {
+    "sid": 28,
+    "depart": "PMC中心",
+    "name": "李德春"
+  },
+  {
+    "sid": 29,
+    "depart": "PMC中心",
+    "name": "徐春林"
+  },
+  {
+    "sid": 30,
+    "depart": "制造技术",
+    "name": "涂圣荣"
+  },
+  {
+    "sid": 31,
+    "depart": "PMC中心",
+    "name": "卢征妮"
+  },
+  {
+    "sid": 32,
+    "depart": "PMC中心",
+    "name": "邵咚"
+  },
+  {
+    "sid": 33,
+    "depart": "PMC中心",
+    "name": "徐会"
+  },
+  {
+    "sid": 34,
+    "depart": "PMC中心",
+    "name": "奚冲"
+  },
+  {
+    "sid": 35,
+    "depart": "PMC中心",
+    "name": "陈梦蕾"
+  },
+  {
+    "sid": 36,
+    "depart": "组装车间",
+    "name": "杨宏"
+  },
+  {
+    "sid": 37,
+    "depart": "PMC中心",
+    "name": "宋挺"
+  },
+  {
+    "sid": 38,
+    "depart": "PMC中心",
+    "name": "王绘"
+  },
+  {
+    "sid": 39,
+    "depart": "精加工车间",
+    "name": "白龙飞"
+  },
+  {
+    "sid": 40,
+    "depart": "组装车间",
+    "name": "石达炜"
+  },
+  {
+    "sid": 41,
+    "depart": "PMC中心",
+    "name": "江俊晴"
+  },
+  {
+    "sid": 42,
+    "depart": "PMC中心",
+    "name": "劳逸超"
+  },
+  {
+    "sid": 43,
+    "depart": "采购中心",
+    "name": "胡杉红"
+  },
+  {
+    "sid": 44,
+    "depart": "设备中心",
+    "name": "龚波"
+  },
+  {
+    "sid": 45,
+    "depart": "制造技术",
+    "name": "项军伟"
+  },
+  {
+    "sid": 46,
+    "depart": "革新中心",
+    "name": "马静"
+  },
+  {
+    "sid": 47,
+    "depart": "PMC中心",
+    "name": "房田田"
+  },
+  {
+    "sid": 48,
+    "depart": "革新中心",
+    "name": "褚肖依"
+  },
+  {
+    "sid": 49,
+    "depart": "组装车间",
+    "name": "王永"
+  },
+  {
+    "sid": 50,
+    "depart": "设备中心",
+    "name": "郑峰"
+  },
+  {
+    "sid": 51,
+    "depart": "设备中心",
+    "name": "刘文超"
+  },
+  {
+    "sid": 52,
+    "depart": "制造技术",
+    "name": "杜洪涛"
+  },
+  {
+    "sid": 53,
+    "depart": "制造技术",
+    "name": "王鑫"
+  },
+  {
+      "sid": 54,
+      "depart": "制造技术",
+      "name": "庄烨"
+  },
+  {
+      "sid": 55,
+      "depart": "PMC中心",
+      "name": "张广泽"
+  },
+  {
+      "sid": 56,
+      "depart": "革新中心",
+      "name": "胡凯"
+  },
+  {
+      "sid": 57,
+      "depart": "PMC中心",
+      "name": "郭向阳"
+  },
+  {
+      "sid": 58,
+      "depart": "设备中心",
+      "name": "陈程"
+  },
+  {
+      "sid": 59,
+      "depart": "设备中心",
+      "name": "黄叶琨"
+  },
+  {
+      "sid": 60,
+      "depart": "制造技术",
+      "name": "乐金勇"
+  },
+  {
+      "sid": 61,
+      "depart": "革新中心",
+      "name": "黎思云"
+  },
+    {
+        "sid": 62,
+        "depart": "革新中心",
+        "name": "王建平"
+    },
+    {
+        "sid": 63,
+        "depart": "革新中心",
+        "name": "张志"
+    },
+    {
+        "sid": 64,
+        "depart": "革新中心",
+        "name": "宋文杰"
+    },
+    {
+        "sid": 65,
+        "depart": "革新中心",
+        "name": "张亮"
+    },
+    {
+        "sid": 66,
+        "depart": "制造技术",
+        "name": "管健宇"
+    },
+    {
+        "sid": 67,
+        "depart": "制造技术",
+        "name": "王围"
+    },
+    {
+        "sid": 68,
+        "depart": "革新中心",
+        "name": "张琪蓉"
+    },
+    {
+        "sid": 69,
+        "depart": "设备中心",
+        "name": "周在禄"
+    }
+    ,
+    {
+        "sid": 70,
+        "depart": "制造技术",
+        "name": "潘晨"
+    },
+    {
+        "sid": 71,
+        "depart": "制造技术",
+        "name": "易赛赛"
+    }
+
+
+
+
+
+];
